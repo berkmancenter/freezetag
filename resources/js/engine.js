06be6a374a96bd1f1a3299baca8b2249bbff7ee5
Tomas Reimers <tomasreimers@gmail.com>
2012-07-02 22:26:13 -0400

    Cleaned up code

--- resources/js/engine.js
+++ resources/js/engine.js
@@ -1,5 +1,11 @@
-// GET VARIABLES
+// ***********************
+// *** GET VARIABLES *****
+// ***********************
+
 // from: http://stackoverflow.com/questions/439463/how-to-get-get-and-post-variables-with-jquery
+
+// parses get variables PHP style
+
 var $_GET = {};
 
 document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
@@ -9,6 +15,9 @@ document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
 
     $_GET[decode(arguments[1])] = decode(arguments[2]);
 });
+
+//  all relevant get variables
+// TODO: Set defaults so code doesn't break if undefined
 
 var url = unescape($_GET["url"]);
 var per_page = parseInt($_GET['per_page']);
@@ -20,7 +29,12 @@ var iterations = parseInt($_GET['iterations']);
 var totalOnPage = parseInt($_GET['total_on_page']);
 var tagsOn = ($_GET['tags'] == 'true' ? true : false);
 
-// ACTUAL FUNCTIONS
+// ***********************
+// *** News Functions ****
+// ***********************
+
+// Actually adds the news item to the DOM
+
 function addItem(title, tags, author, date, link){
 	var tagString = "";
 	for (var i = 0; i < tags.length && tagsOn; i++){
@@ -47,6 +61,8 @@ function addItem(title, tags, author, date, link){
 	}
 }
 
+// gets news via API, first a function which makes the request and then the jsonp callback
+
 var feed = new Array();
 
 function fetchNews(){
@@ -65,8 +81,10 @@ function fetchNewsComplete(data){
 	dropNews();
 }
 
+// var used as an end point to determine whether news is old news
 var ageMax = 5; // in days
 
+// removes news item from queue to display, then it cals itself again
 function dropNews(){
 	// get news item
 	var currItem = feed.shift();
@@ -89,11 +107,11 @@ function dropNews(){
 		if (dayDiff > ageMax){
 			dayDiff = ageMax;
 		}
-		// TODO: possibly remove
+		// TODO: possibly remove, or find use for it
 		var ratio = dayDiff / ageMax;
 		// add it
 		addItem(currItem.title, tags, author, dateStr, currItem.url);
-		// set it to occur again
+		// loop
 		setTimeout(dropNews, pauseTime + slideAnimateTime + fadeAnimateTime);
 	}
 	else {
@@ -104,12 +122,3 @@ function dropNews(){
 $(document).ready(function (){
 	fetchNews('fetchNewsComplete');
 });
-
-function getTime(feedItem){
-	var dateObj = new Date(feedItem.date_published);
-	var date = dateObj.getTime();
-	return date;
-}
-
-var currentTailPage;
-var loadingTailEnd = false;
